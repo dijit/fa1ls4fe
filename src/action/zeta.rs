43f4a3b2c9fb70d776661994dcee0a3678924e02
Jan Harasym <jharasym@linux.com>
2020-07-12 02:19:40 +0200

    updating dependencies, adding assertion when incorrect compile

--- src/action/zeta.rs
+++ src/action/zeta.rs
@@ -1,11 +1,11 @@
-extern crate ssh;
 extern crate const_env;
+extern crate ssh;
 
 use self::const_env::from_env;
 use self::ssh::*;
+use std::assert;
 use std::io::Read;
 use std::io::Write;
-
 
 #[from_env("SSH_HOST")]
 const HOST: &'static str = "placeholder_hostname";
@@ -13,12 +13,17 @@ const HOST: &'static str = "placeholder_hostname";
 const USER: &'static str = "placeholder_username";
 #[from_env("SSH_PASS")]
 const PASS: &'static str = "placeholder_password";
-
 pub fn send_script() {
     /* Goal here is to upload the shell script we will execute later
      * We could read this in from a file, but that would mean
      * that I have to ship failsafe as more than just a single binary
      */
+
+    assert!(
+        USER != "placeholder_username".to_string(),
+        "No SSH_USER env var was defined at compile time"
+    );
+
     let script: &'static str = r#"#!/usr/bin/env bash
 set -u
 set -o noglob
@@ -58,7 +63,7 @@ else
     echo -n 'Zeta running (automated start)'
 fi
 "#;
-    let mut session=Session::new().unwrap();
+    let mut session = Session::new().unwrap();
     session.set_host(&HOST).unwrap();
     session.parse_config(None).unwrap();
     match session.connect() {
@@ -66,74 +71,67 @@ fi
             session.set_username(&USER).unwrap();
             //session.userauth_publickey_auto(None).unwrap();
             match session.userauth_password(&PASS) {
-                Ok(_) => {},
+                Ok(_) => {}
                 Err(_) => return,
             }
             {
-                let mut scp = session.scp_new(
-                        WRITE, 
-                        "/Users/jharasym/projects/external/errbot/")
+                let mut scp = session
+                    .scp_new(WRITE, "/Users/jharasym/projects/external/errbot/")
                     .unwrap();
                 scp.init().unwrap();
-                let buf=script.as_bytes().to_vec();
-                scp.push_file("ensure_alive.sh",buf.len(),0o700).unwrap();
+                let buf = script.as_bytes().to_vec();
+                scp.push_file("ensure_alive.sh", buf.len(), 0o700).unwrap();
                 scp.write(&buf).unwrap();
             }
-        },
+        }
         Err(error) => eprintln!("[✗] failed to transfer script: {:?}", error),
     }
-
 }
 
 pub fn try_start() -> (bool, String) {
     /* Goal of this function is to SSH to my mac
      * And run a special idempotent shell/python shell shit
      * to make sure zeta is running and responding.
-     * if this function returns, we better make damned sure 
+     * if this function returns, we better make damned sure
      * zeta is running. Panicking here is not an option.
      */
     let mut success = false;
-    let mut session=Session::new().unwrap();
+    let mut session = Session::new().unwrap();
     session.set_host(&HOST).unwrap();
     session.set_username(&USER).unwrap();
     session.parse_config(None).unwrap();
     match session.connect() {
-        Ok(_) => {},
+        Ok(_) => {}
         Err(error) => return (false, error.to_string()),
     }
 
     //println!("{:?}",session.is_server_known());
 
     match session.userauth_password(&PASS) {
-        Ok(_) => {},
+        Ok(_) => {}
         Err(error) => return (false, error.to_string()),
     }
     {
-        let mut s=session.channel_new().unwrap();
+        let mut s = session.channel_new().unwrap();
         s.open_session().unwrap();
-        s.request_exec(
-            b"/Users/jharasym/projects/external/errbot/ensure_alive.sh")
+        s.request_exec(b"/Users/jharasym/projects/external/errbot/ensure_alive.sh")
             .unwrap();
         let exit = s.get_exit_status().unwrap();
         match exit {
-            0 => success=true,
+            0 => success = true,
             _ => println!("[✗] Script exit status is: {:?}", exit),
         }
         s.send_eof().unwrap();
-        let mut buf=Vec::new();
+        let mut buf = Vec::new();
         if success {
             s.stdout().read_to_end(&mut buf).unwrap();
         } else {
             // FIXME: this will crash if there's nothing in the buffer.
             match s.stderr().read_to_end(&mut buf) {
-                Ok(_) => {},
+                Ok(_) => {}
                 Err(error) => println!("Got error while trying to read stderr: {:?}", error),
             }
         }
-        return (
-            success,
-            format!("{:?}", std::str::from_utf8(&buf).unwrap())
-        );
+        return (success, format!("{:?}", std::str::from_utf8(&buf).unwrap()));
     }
-
 }
--- src/main.rs
+++ src/main.rs
@@ -7,30 +7,37 @@ use std::{env, process};
 
 fn main() {
     println!(
-"   ___     _ _ __        __      
+        "   ___     _ _ __        __      
   / __\\_ _(_) / _\\ __ _ / _| ___ 
  / _\\/ _` | | \\ \\ / _` | |_ / _ \\
 / / | (_| | | |\\ \\ (_| |  _|  __/
 \\/   \\__,_|_|_\\__/\\__,_|_|  \\___|
  Zeta Integrity Protection System
 "
-);
+    );
+    let mut help_output: bool = false;
     let args: Vec<String> = env::args().collect();
     for arg in args {
         if arg.contains(&"--help") {
-            println!("☶ Protocol 1: link to bot");
-            println!("☵ Protocol 2: uphold the mission");
-            println!("☳ Protocol 3: protect the bot");
-            println!("                              ℐℋ");
+            help_output = true;
+        } else if arg.contains(&"-h") {
+            help_output = true;
         }
+    }
+    if help_output {
+        println!("☶ Protocol 1: link to bot");
+        println!("☵ Protocol 2: uphold the mission");
+        println!("☳ Protocol 3: protect the bot");
+        println!("                              ℐℋ");
+        process::exit(0);
     }
     let api_key: String = api_key();
     let mut handler = handler::Handler;
     let r = RtmClient::login_and_run(&api_key, &mut handler);
 
     match r {
-        Ok(_) => {},
-        Err(err) => panic!("Error: {}", err)
+        Ok(_) => {}
+        Err(err) => panic!("Error: {}", err),
     }
 }
 
